23684f09bfdada64d7fcc4e0b3e9b9856852cb7a
Kholmanov Andrei Work <ankholmanov@yandex.ru>
2020-11-17 09:03:22 +0700

    edit design support

--- resources/views/supports/index.blade.php
+++ resources/views/supports/index.blade.php
@@ -37,48 +37,53 @@ $breadcrumbs = [
                         <a class="btn btn-primary" href="{{ route('support.create') }}">{{ __('Написать в поддержку') }}</a>
                     </div>
                 @endif
-                <section>
-                    <div class="row item-font">
-                        <div class="col-sm-2">№</div>
-                        <div class="col-sm-4">Наименование</div>
-                        <div class="col-sm-2">Тема</div>
-                        <div class="col-sm-2"></div>
-                    </div>
-                    @foreach($supports as $item)
-                        <div class="row item-font">
-                            <div class="col-sm-2">SUP{{ $item->id }}</div>
-                            <div class="col-sm-4">{{ $item->title }}</div>
-                            <div class="col-sm-2">
-                                @if($item->order)
-                                    {{ $item->order->title }}
-                                @endif
-                                @if($item->parcel)
-                                    {{ $item->parcel->title }}
-                                @endif
-                                @if(!$item->parcel && !$item->order)
-                                    {{ __('Общий') }}
-                                @endif
-                            </div>
-                            <div class="col-sm-2">
-                                <div class="col-sm-2">
-                                    <div class="row">
-                                        <a class="btn btn-primary mr-2" href="{{ route('support.show', $item) }}">{{ __('Перейти') }}</a>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th scope="col" class="align-middle text-center">#</th>
+                            <th scope="col" class="align-middle">Наименование</th>
+                            <th scope="col" class="align-middle text-center">Тема</th>
+                            <th scope="col" class="align-middle text-center">Действие</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($supports as $item)
+                            <tr>
+                                <th scope="row" class="align-middle text-center">SUP{{ $item->id }}</th>
+                                <td class="align-middle">{{ $item->title }}</td>
+                                <td class="align-middle text-center">
+                                    @if($item->order)
+                                        {{ $item->order->title }}
+                                    @endif
+                                    @if($item->parcel)
+                                        {{ $item->parcel->title }}
+                                    @endif
+                                    @if(!$item->parcel && !$item->order)
+                                        {{ __('Общий') }}
+                                    @endif
+                                </td>
+                                <td class="align-middle text-center">
+                                    <div class="d-flex justify-content-center">
+                                        <a class="btn btn-primary mr-2" href="{{ route('support.show', $item) }}" title="Перейти в запрос">
+                                            <i class="czi-document align-middle"></i></a>
                                         @if(Auth::user()->hasRole('manager') && !$item->manager_id)
                                             <form class="row" method="POST"
                                                   action="{{ route('manager.support-accept', $item) }}">
                                                 @csrf
                                                 @method('PUT')
-                                                <button type="submit" class="btn btn-primary item__link">
-                                                    {{ __('Принять') }}
+                                                <button type="submit" class="btn btn-primary" title="Принять в работу">
+                                                    <i class="czi-add align-middle"></i>
                                                 </button>
                                             </form>
                                         @endif
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </section>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
